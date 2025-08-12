d1ef4cc1f23ddc5260053ec9d6070e7f5dd4f672
Shayan Alam <ishayanalam@gmail.com>
2025-08-12 12:30:51 +0600

    fixed components

--- browse_food.php
+++ browse_food.php
@@ -1,5 +1,29 @@
 <?php include 'components/header.php'; ?>
 <?php include 'components/navbar.php'; ?>
 
-<h1>gg</h1>
+<div class="category-list">
+  <div class="each-category">
+    <img src="assets/categories/Baked.png" />
+    <p class="category-name">Bakery</p>
+  </div>
+  <div class="each-category">
+    <img src="assets/categories/Burger.png" />
+    <p class="category-name">Burger</p>
+  </div>
+  <div class="each-category">
+    <img src="assets/categories/Chicken.png" />
+    <p class="category-name">Chicken</p>
+  </div>
+  <div class="each-category">
+    <img src="assets/categories/Coffee.png" />
+    <p class="category-name">Baverage</p>
+  </div>
+  <div class="each-category">
+    <img src="assets/categories/Fish.png" />
+    <p class="category-name">Seafood</p>
+  </div>
+</div>
+
+<h1>Chefs Special</h1>
+
 <?php include 'components/footer.php'; ?>
--- explore_menue.php
+++
@@ -1,6 +0,0 @@
-<?php include 'components/header.php'; ?>
-<?php include 'components/navbar.php'; ?>
-
-
-<h1>Food</h1>
-<?php include 'components/footer.php'; ?>
