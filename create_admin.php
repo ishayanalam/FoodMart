97cd6b191012357741501279db618d944b40d073
zahraaa003 <zahra.rahman.cse@ulab.edu.bd>
2025-08-21 14:08:57 +0600

    Added hashed password

---
+++ create_admin.php
@@ -0,0 +1,10 @@
+<?php
+include 'db_connection.php';
+
+$username = "admin1";
+$password = "mypassword123";
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO admin (username, password) VALUES ('$username', '$hashedPassword')";
+$connection->query($sql);
+?>
