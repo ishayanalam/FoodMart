3c580194f4edb99cd65ae6e8cc6fa109e0d12608
Shayan Alam <ishayanalam@gmail.com>
2025-08-27 00:51:38 +0600

    admin can manage the order: received

--- components/admin/admin_navbar.php
+++ components/admin/admin_navbar.php
@@ -5,7 +5,7 @@
         <li><a href="#" class="active">Dashboard</a></li>
 
         <li class="nav-heading">Order Options</li>
-        <li><a href="#">Order Received</a></li>
+        <li><a href="admin_order_received.php">Order Received</a></li>
         <li><a href="#">Order Confirmed</a></li>
         <li><a href="#">Ready for Delivery</a></li>
 
